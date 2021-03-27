6024be96fbdb540a9a8c6c87a5579310d1b11939
michal.skiba.2000@gmail.com <michal.skiba.2000@gmail.com>
2021-03-27 18:00:14 +0100

    Home init

---
+++ src/modules/home/components/FeedPost/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./styles.scss";
+
+class FeedPost extends React.Component {
+    render() {
+        return (
+            <div className="fedd-post">
+              <div className="post-info">
+                Post created by (username) (time_ago)
+              </div>
+              <div className="post-title">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus facilisis sit amet quam vitae mattis.
+              </div>
+              <div className="post-img">
+                <img src="https://images-na.ssl-images-amazon.com/images/I/71LaTazW6eL._AC_SL1500_.jpg"/>
+              </div>
+            </div>
+        );
+    }
+}
+
+export default FeedPost;
