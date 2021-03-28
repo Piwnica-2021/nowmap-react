7719a358115b3712f2aab29269516eff12fe161d
Carbon225 <carbon225@protonmail.com>
2021-03-28 04:47:01 +0200

    map center

--- src/modules/home/components/Map/index.js
+++ src/modules/home/components/Map/index.js
@@ -1,31 +1,31 @@
-import React, { Component } from 'react';
-import GoogleMapReact  from 'google-map-react';
+import React, {Component} from 'react';
+import GoogleMapReact from 'google-map-react';
 import Marker from "../Marker";
 import "./styles.scss";
 
 class Map extends Component {
-  render() {
-    return (
-        <div className="map-container">
-          <GoogleMapReact
-              bootstrapURLKeys={{ key: "AIzaSyAUUR3rmebpIm98TxMmXkPZy6wjn2IOfV8" }}
-              defaultCenter={{ lat: 50.6, lng: 19.93333 }}
-              defaultZoom={12}
-          >
-            {this.props.posts.map((post, index) => {
-              return(
-                  <Marker
-                      lat={post.latitude}
-                      lng={post.longitude}
-                      name="My Marker"
-                      color="blue"
-                  />
-              )
-            })}
-          </GoogleMapReact>
-        </div>
-    );
-  }
+    render() {
+        return (
+            <div className="map-container">
+                <GoogleMapReact
+                    bootstrapURLKeys={{key: "AIzaSyAUUR3rmebpIm98TxMmXkPZy6wjn2IOfV8"}}
+                    defaultCenter={{lat: 50.0640, lng: 19.9276}}
+                    defaultZoom={12}
+                >
+                    {this.props.posts.map((post, index) => {
+                        return (
+                            <Marker
+                                lat={post.latitude}
+                                lng={post.longitude}
+                                name="My Marker"
+                                color="blue"
+                            />
+                        )
+                    })}
+                </GoogleMapReact>
+            </div>
+        );
+    }
 }
 
 export default Map;
